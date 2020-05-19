c269019f1bb6c1bff55a08aa3d36f7af8d32956d
Amalnnath Parameswaran <aparameswaran@qaconsultants.com>
2020-05-19 14:27:15 -0400

    merge

--- python_headless/receiver.py
+++ python_headless/receiver.py
@@ -4,6 +4,40 @@ import cantools
 import can
 import socket
 
+# code modified, tweaked and tailored from code by bertwert 
+# on RPi forum thread topic 91796
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+ 
+# GPIO ports for the 7seg pins
+segments =  (11,4,23,8,7,10,18,25)
+# 7seg_segment_pins (11,7,4,2,1,10,5,3) +  100R inline
+ 
+for segment in segments:
+    GPIO.setup(segment, GPIO.OUT)
+    GPIO.output(segment, 0)
+ 
+# GPIO ports for the digit 0-3 pins 
+digits = (22,27,17,24)
+# 7seg_digit_pins (12,9,8,6) digits 0-3 respectively
+ 
+for digit in digits:
+    GPIO.setup(digit, GPIO.OUT)
+    GPIO.output(digit, 1)
+ 
+num = {' ':(0,0,0,0,0,0,0),
+    '0':(1,1,1,1,1,1,0),
+    '1':(0,1,1,0,0,0,0),
+    '2':(0,1,0,1,1,1,1),
+    '3':(0,1,1,1,0,1,1),
+    '4':(1,1,1,0,0,0,1),
+    '5':(1,0,1,1,0,1,1),
+    '6':(1,0,1,1,1,1,1),
+    '7':(0,1,1,0,0,1,0),
+    '8':(1,1,1,1,1,1,1),
+    '9':(1,1,1,1,0,1,1)}
+
 db = cantools.database.load_file('../nissan_leaf_2018.dbc')
 can_bus = can.interface.Bus('vcan0', bustype='socketcan')
 
@@ -19,7 +53,19 @@ while(True):
     message = can_bus.recv()
 
     if(message.arbitration_id == 645):
-        print("Speed: " + str( db.decode_message(message.arbitration_id, message.data)['WHEEL_SPEED_RR']))
+        speed = str( db.decode_message(message.arbitration_id, message.data)['WHEEL_SPEED_RR'])
+        print("Speed: " + speed)
+        s = speed.rjust(4)
+        for digit in range(4):
+            for loop in range(0,7):
+                GPIO.output(segments[loop], num[s[digit]][loop])
+                if (int(time.ctime()[18:19])%2 == 0) and (digit == 1):
+                    GPIO.output(25, 1)
+                else:
+                    GPIO.output(25, 0)
+            GPIO.output(digits[digit], 0)
+            time.sleep(0.001)
+            GPIO.output(digits[digit], 1)
 
     elif(message.arbitration_id == 1057):
         print("Gears: " + db.decode_message(message.arbitration_id, message.data)['GEAR_SHIFTER'])
